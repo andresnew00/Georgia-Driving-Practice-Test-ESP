2a982a707f2002be5a0716155c9f7b7f083803d6
andresnew00 <andresnew00@hotmail.com>
2020-07-08 18:14:06 -0400

    basic layout completed need to add more questions and a way to display when the answer is wrong

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,90 @@
+var quizQuestions = [
+  {
+    question: "Cuando manejas de noche, debes bajar las luces cuando:",
+    answers: [
+      {
+        type: "Incorrect",
+        content: "Cuando las luces del otro vehiculo estan bajas",
+      },
+      {
+        type: "Correct",
+        content: "Cuando te encuentras con otro vehiculo",
+      },
+      {
+        type: "Incorrect",
+        content: "Cuando te acercas a una interseccion",
+      }
+    ],
+  },
+  {
+    question: "La ley de manos libres de Georgia le aplica a que de lo siguiente:",
+    answers: [
+      {
+        type: "Correct",
+        content: "Todos los conductores no importa la edad.",
+      },
+      {
+        type: "Incorrect",
+        content: "Conductores Ancianos",
+      },
+      {
+        type: "Incorrect",
+        content: "Conductores menores de 21",
+      },
+    ],
+  },
+  {
+    question:
+      "La ley de 'Move-Over' implica que los conductores:",
+    answers: [
+      {
+        type: "Correct",
+        content: "Moverse al menos una linea alejado de un vehiculo de emergencia que esta estacionado, o si no puede moverse, bajar la velocidad por debajo del limite de velocidad.",
+      },
+      {
+        type: "Incorrect",
+        content: "Mantenerce en la linea derecha si manejas un tractor o trailer",
+      },
+      {
+        type: "Incorrect",
+        content: "Quitar vehiculos fuera de las lineas de trafico que siguen un accidente.",
+      },
+    ],
+  },
+  {
+    question: "Si te ves envuelta en un accidente, uno de los requerimientos principales es:",
+    answers: [
+      {
+        type: "Correct",
+        content: "Bridar ayuda a los heridos.",
+      },
+      {
+        type: "Incorrect",
+        content: "Notificar al setretario de estado.",
+      },
+      {
+        type: "Incorrect",
+        content: "Notificar a tu corredor de seguros.",
+      },
+    ],
+  },
+  {
+    question: "Que deberias hacer cuando ves una senal de trafico amarilla parpadeando en la siguiente interseccion?",
+    answers: [
+      {
+        type: "Correct",
+        content: "Baja la velocidad y procede con precaucion.",
+      },
+      {
+        type: "Incorrect",
+        content: "Acelera antes de que la luz cambie a rojo.",
+      },
+      {
+        type: "Incorrect",
+        content: "detenerse por completo antes de proceder.",
+      },
+    ],
+  },
+];
+
+export default quizQuestions;
